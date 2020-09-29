367b62bb79699f4f8d4a942197b3b23f29ea7b2e
rafael ubuntu <ryikeda@outlook.com>
2020-09-30 04:25:55 +0900

    Updates

---
+++ precache-manifest.e84a0b24f38014e860f7f30ba430ac27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de8a55379ff800aebd4ec8647291c28",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "c9ed52a3773f06dd112f",
+    "url": "/netflix-clone/static/css/main.b599f97f.chunk.css"
+  },
+  {
+    "revision": "d04aefe66cd3d9e7aa97",
+    "url": "/netflix-clone/static/js/2.1844a557.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/netflix-clone/static/js/2.1844a557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ed52a3773f06dd112f",
+    "url": "/netflix-clone/static/js/main.dc8df0ed.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  },
+  {
+    "revision": "a97a75c6c35369617a1561e2a2c6bef4",
+    "url": "/netflix-clone/static/media/logo.a97a75c6.png"
+  },
+  {
+    "revision": "7bcb00951048e6114939f4a0c366c79a",
+    "url": "/netflix-clone/static/media/spinner.7bcb0095.gif"
+  }
+]);
