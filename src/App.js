8235bdcfd81085b91f0ecfc7cd569ac3bb43ab41
rafael ubuntu <ryikeda@outlook.com>
2020-09-29 22:08:32 +0900

    created file to fetch API data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Tmdb from "./Tmdb";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const loadData = async () => {
+      // Get full movies list
+      const moviesList = await Tmdb.getMoviesList();
+      console.log(moviesList);
+    };
+    loadData();
+  }, []);
   return <div className="App">Initial setup</div>;
 }
 
---
+++ src/Tmdb.js
@@ -0,0 +1,79 @@
+/* Handles API calls*/
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = "0ac387581772dda1f4d5545db27281c3";
+
+/*
+ - trending
+ - top rated
+ - action
+ - comedy
+ - horror
+ - romance
+ - documentaries
+*/
+
+const fetchData = async (endpoint) => {
+  const res = await fetch(`${BASE_URL}${endpoint}`);
+  const json = await res.json();
+  return json;
+};
+
+export default {
+  getMoviesList: async () => {
+    return [
+      {
+        slug: "originals",
+        title: "Netflix Originals",
+        items: await fetchData(
+          `/discover/tv?with_network=213&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "trending",
+        title: "Trending",
+        items: await fetchData(`/trending/all/week?api_key=${API_KEY}`),
+      },
+      {
+        slug: "toprated",
+        title: "Top Rated",
+        items: await fetchData(`/movie/top_rated?api_key=${API_KEY}`),
+      },
+      {
+        slug: "action",
+        title: "Action",
+        items: await fetchData(
+          `/discover/movie?with_genres=28&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "comedy",
+        title: "Comedy",
+        items: await fetchData(
+          `/discover/movie?with_genres=35&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "horror",
+        title: "Horror",
+        items: await fetchData(
+          `/discover/movie?with_genres=27&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "romance",
+        title: "Romance",
+        items: await fetchData(
+          `/discover/movie?with_genres=10749&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "documentary",
+        title: "Documentatios",
+        items: await fetchData(
+          `/discover/movie?with_genres=99&api_key=${API_KEY}`
+        ),
+      },
+    ];
+  },
+};
